9c1d3176ba92ea86dda599988b17f26fdf1b484b
vishalbodkhe531 <vishalbodkhe531@gmail.com>
2026-03-10 21:37:43 +0530

    completed basic setup of backend

---
+++ backend/src/server.ts
@@ -0,0 +1,17 @@
+import express from "express";
+import cors from "cors";
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("API running");
+});
+
+app.listen(5000, () => {
+  console.log("Server running on port 5000");
+});
+
+export default app;
